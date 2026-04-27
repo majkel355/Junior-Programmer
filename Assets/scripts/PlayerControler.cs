3cb90da4b0d4d64c4e3133758b27cf8d4720e5ff
majkel355 <129754260+majkel355@users.noreply.github.com>
2026-04-27 21:48:46 +0200

    script changes
    Movement speed has been changed, the ability to drive and turn the vehicle has been added

--- Assets/scripts/PlayerControler.cs
+++ Assets/scripts/PlayerControler.cs
@@ -7,11 +7,17 @@ public class PlayerControler : MonoBehaviour
     {
         
     }
-    public float speed = 5.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed *horizontalInput * Time.deltaTime);
     }
 }
