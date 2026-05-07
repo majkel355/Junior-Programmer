4cbd3fb0d8f7e902c23aaa1e136b45b83e59afa1
majkel355 <129754260+majkel355@users.noreply.github.com>
2026-05-07 14:45:07 +0200

    serializeField and camera change
    in the script for the player character and for the camera I added SerializeField before the speed and rotation speed values ​​and removed the void start

--- Assets/scripts/PlayerControler.cs
+++ Assets/scripts/PlayerControler.cs
@@ -2,17 +2,12 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-    private float speed = 20.0f;
-    private float turnSpeed = 45.0f;
+    [SerializeField] private float speed = 20.0f;
+    [SerializeField] private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
